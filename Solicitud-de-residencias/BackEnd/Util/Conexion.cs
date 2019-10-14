3159f5c6c65364f386a021b62c1c304503ad7cff
Jose <963789tkG>
2019-10-14 18:56:07 -0500

    Conexión a base de datos

--- Solicitud-de-residencias/BackEnd/Util/Conexion.cs
+++ Solicitud-de-residencias/BackEnd/Util/Conexion.cs
@@ -1,12 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace BackEnd.Util
 {
-    class Conexion
+    public class Conexion
     {
+        public static MySqlConnection conexion = null;
+
+        static public bool Conectar()
+        {
+            String parametrosConexion = "Server=127.0.0.1;Database=solicitudResidencias;Uid=root;Pwd=root;Port=3306;";
+
+            // String parametrosConexion = "Server=mysql5014.site4now.net;Database=db_a4ac5e_ptmbd;Uid=a4ac5e_ptmbd;Pwd=anjoluande7.s;";
+            conexion = new MySqlConnection(parametrosConexion);
+            try
+            {
+                conexion.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+        public DataTable ejecutarConsulta(MySqlCommand com)
+        {
+            try
+            {
+                if (Conectar())
+                {
+                    com.Connection = conexion;
+                    MySqlDataAdapter objAdapter = new MySqlDataAdapter(com);
+                    DataTable resultado = new DataTable();
+                    objAdapter.Fill(resultado);
+                    return resultado;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+
+        }
+
+
+        public int ejecutarSentencia(MySqlCommand objComando, bool esInsert)
+        {
+            int resultado = 0;
+            try
+            {
+                if (Conectar())
+                {
+                    objComando.Connection = conexion;
+                    MySqlDataAdapter objAdapter = new MySqlDataAdapter(objComando);
+                    if (esInsert)
+                    {
+                        resultado = int.Parse(objComando.ExecuteScalar().ToString());
+                    }
+                    else
+                    {
+                        objComando.ExecuteNonQuery();
+                        resultado = 1;
+                    }
+                    return resultado;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+            finally
+            {
+                if (conexion != null)
+                    conexion.Close();
+            }
+        }
+
+
+
     }
 }
