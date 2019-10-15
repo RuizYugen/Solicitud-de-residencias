2702cb28482e5a48b4440deb0006091cf2846654
José <jjosee961310@gmail.com>
2019-10-15 18:45:37 -0500

    Corrección clase conexión.

--- Solicitud-de-residencias/BackEnd/Util/Conexión.cs
+++ Solicitud-de-residencias/BackEnd/Util/Conexión.cs
@@ -10,83 +10,96 @@ namespace BackEnd.Util
 {
     public class Conexion
     {
-        public string GetConnectionString()
+        public static MySqlConnection conexion = null;
+
+        static public bool Conectar()
         {
-            //Revisar: https://www.connectionstrings.com/mysql/
+            String parametrosConexion = "Server=127.0.0.1;Database=solicitudResidencias;Uid=root;Pwd=root;Port=3306;";
 
-            String servidor = "localhost";
-            String puerto = "3306";
-            String usuario = "root";
-            String password = "root";
-            String database = "itsurenglishlearn";
 
-            //Cadena de conexion
-
-            return String.Format("server={0};port={1};user id={2}; password={3}; database={4}", servidor, puerto, usuario, password, database);
-        }
-
-        public void EjecutaSQLComando(MySqlCommand sqlCom)
-        {
-            MySqlConnection sqlConn = new MySqlConnection();
-            //MySqlCommand sqlCom = new MySqlCommand();
+            conexion = new MySqlConnection(parametrosConexion);
             try
             {
-                sqlConn.ConnectionString = GetConnectionString();
-                sqlConn.Open();
-                sqlCom.Connection = sqlConn;
-                //sqlCom.CommandText = sComandoSql;
-                sqlCom.ExecuteNonQuery();
+                conexion.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+
+        public DataTable ejecutarConsulta(MySqlCommand com)
+        {
+            try
+            {
+                if (Conectar())
+                {
+                    com.Connection = conexion;
+                    MySqlDataAdapter objAdapter = new MySqlDataAdapter(com);
+                    DataTable resultado = new DataTable();
+                    objAdapter.Fill(resultado);
+                    return resultado;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                return null;
             }
             finally
             {
-                sqlConn.Close();
+                if (conexion != null)
+                    conexion.Close();
             }
+
         }
 
-        public DataSet LLenaComboGrid(string mysql)
+
+        public int ejecutarSentencia(MySqlCommand objComando, bool esInsert)
         {
-            DataSet ds = new DataSet();
-            MySqlDataAdapter da = new MySqlDataAdapter();
-            MySqlConnection cnn = new MySqlConnection();
-            MySqlCommand cmd = new MySqlCommand();
+            int resultado = 0;
             try
             {
-                cnn.ConnectionString = GetConnectionString();
-                cnn.Open();
-                cmd.CommandText = mysql;
-                da.SelectCommand = cmd;
-                da.SelectCommand.Connection = cnn;
-                da.Fill(ds);
+                if (Conectar())
+                {
+                    objComando.Connection = conexion;
+                    MySqlDataAdapter objAdapter = new MySqlDataAdapter(objComando);
+                    if (esInsert)
+                    {
+                        resultado = int.Parse(objComando.ExecuteScalar().ToString());
+                    }
+                    else
+                    {
+                        objComando.ExecuteNonQuery();
+                        resultado = 1;
+                    }
+                    return resultado;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                return 0;
             }
             finally
             {
-
-                cnn.Close();
+                if (conexion != null)
+                    conexion.Close();
             }
-            return ds;
         }
 
-        public DataSet LLenaComboGrid(MySqlCommand cmd)
-        {
-            DataSet ds = new DataSet();
-            MySqlDataAdapter da = new MySqlDataAdapter();
-            MySqlConnection cnn = new MySqlConnection();
-            try
-            {
-                cnn.ConnectionString = GetConnectionString();
-                cnn.Open();
-                //cmd.CommandText = mysql;
-                da.SelectCommand = cmd;
-                da.SelectCommand.Connection = cnn;
-                da.Fill(ds);
-            }
-            finally
-            {
 
-                cnn.Close();
-            }
-            return ds;
-        }
+
     }
+
 }
 
